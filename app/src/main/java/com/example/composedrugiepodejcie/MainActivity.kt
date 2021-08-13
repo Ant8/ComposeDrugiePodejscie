b4444f8bdafdeade708211e3188f0ea5104b6eee
Antoni Moroz <antek@codequest.com>
2021-08-13 17:02:30 +0200

    use row and add image, use modifiers

--- app/src/main/java/com/example/composedrugiepodejcie/MainActivity.kt
+++ app/src/main/java/com/example/composedrugiepodejcie/MainActivity.kt
@@ -3,10 +3,23 @@ package com.example.composedrugiepodejcie
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composedrugiepodejcie.ui.theme.ComposeDrugiePodejścieTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,16 +34,31 @@ class MainActivity : ComponentActivity() {
 private fun MessageContent(
     message: Message = Message()
 ) {
-    Column {
-        Text(message.author)
-        Text(message.body)
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "igonga",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(message.author)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(message.body)
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewMessageContent() {
-    MessageContent()
+    ComposeDrugiePodejścieTheme {
+        MessageContent()
+    }
 }
 
 private data class Message(
