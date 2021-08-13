3200c6af123c1fd76fa8311c6e476d7d2636974d
Antoni Moroz <antek@codequest.com>
2021-08-13 17:09:20 +0200

    some styling and typography

--- app/src/main/java/com/example/composedrugiepodejcie/MainActivity.kt
+++ app/src/main/java/com/example/composedrugiepodejcie/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,10 +13,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +28,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageContent(Message("wesół awtor", "wesoła treźdź"))
+            ComposeDrugiePodejścieTheme {
+                MessageContent(Message("wesół awtor", "wesoła treźdź"))
+            }
         }
     }
 }
@@ -41,14 +46,23 @@ private fun MessageContent(
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colors.secondaryVariant, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(message.author)
+            Text(
+                message.author,
+                modifier = Modifier.border(2.dp, MaterialTheme.colors.secondaryVariant, CircleShape),
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(message.body)
+            Text(
+                message.body,
+                color = MaterialTheme.colors.error,
+                style = MaterialTheme.typography.body1
+            )
         }
     }
 }
